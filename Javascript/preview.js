7e9e73e7bebfd1600d231f3bf7cfb2f3c6b350cf
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2024-05-04 17:16:03 +0530

    Added Header Queries and Routes Parameters

--- Javascript/preview.js
+++ Javascript/preview.js
@@ -31,7 +31,149 @@ var resBody = document.getElementsByClassName('res-body')[0];
     }
 
 
+//showing the parameters Headers
+if(parseInt(jsonData["headers"][0].header)>0){
+    var headerName = []
+    var headerType = []
+    //it means header exists
+    Array.from(jsonData["headers"]).forEach((item,index)=>{
+    if(index!=0)
+        {
+            const obj = jsonData["headers"][index];
+            if(index%2==0)
+                {
+                    headerType.push(Object.values(obj)[0])
+                }else{
+                    headerName.push(Object.values(obj)[0])
+                }
+        }
+    })
+var className;
+  //showing total headers
+  document.getElementsByClassName("totalHeaders")[0].innerText = "Total Headers " + jsonData["headers"][0].header;
+  //now showing the header name with type
+//   we can take the reference of array headertype or headeName coz both are having same length
+headerName.forEach((item, index)=>{
+    if(item=="")
+        item= "Name Not Provided"
+    if(headerType[index]=="string")
+        className = "info"
+    else if(headerType[index]== "int")
+        className ="secondary"
+    else if(headerType[index]== "bool")
+        className = "success"
+    else
+        className = "danger"
 
+ 
+    document.getElementsByClassName("insertHeaderinfo")[0].innerHTML += `
+    <div class="row mt-2">
+    <div class="col-sm-6">
+        <p><b> Header-${index+1} -- ${item} </b></p>
+    </div>
+    <div class="col-sm-6">
+        <button class="btn btn-${className}" style="height:40px; width:100px;">${headerType[index]}</button>
+    </div>
+ </div>
+    `;
+})
+};
+
+//showing the Queries
+if(parseInt(jsonData["queries"][0].query)>0){
+    var QueryName = []
+    var QueryType = []
+    //it means header exists
+    Array.from(jsonData["queries"]).forEach((item,index)=>{
+    if(index!=0)
+        {
+            const obj = jsonData["queries"][index];
+            if(index%2==0)
+                {
+                    QueryType.push(Object.values(obj)[0])
+                }else{
+                    QueryName.push(Object.values(obj)[0])
+                }
+        }
+    })
+var className;
+  //showing total headers
+  document.getElementsByClassName("totalQuery")[0].innerText = "Total Queries " + jsonData["queries"][0].query;
+  //now showing the header name with type
+//   we can take the reference of array querytype or queryName coz both are having same length
+QueryName.forEach((item, index)=>{
+    if(item=="")
+        item= "Name Not Provided"
+    if(QueryType[index]=="string")
+        className = "info"
+    else if(QueryType[index]== "int")
+        className ="secondary"
+    else if(QueryType[index]== "bool")
+        className = "success"
+    else
+        className = "danger"
+
+ 
+    document.getElementsByClassName("insertQueryinfo")[0].innerHTML += `
+    <div class="row mt-2">
+    <div class="col-sm-6">
+        <p><b> Query-${index+1} -- ${item} </b></p>
+    </div>
+    <div class="col-sm-6">
+        <button class="btn btn-${className}" style="height:40px; width:100px;">${QueryType[index]}</p>
+    </div>
+ </div>
+    `;
+})
+};
+
+//showing Routes
+if(parseInt(jsonData["routes"][0].route)>0){
+    var RouteName = []
+    var RouteType = []
+    //it means header exists
+    Array.from(jsonData["routes"]).forEach((item,index)=>{
+    if(index!=0)
+        {
+            const obj = jsonData["routes"][index];
+            if(index%2==0)
+                {
+                    RouteType.push(Object.values(obj)[0])
+                }else{
+                    RouteName.push(Object.values(obj)[0])
+                }
+        }
+    })
+var className;
+  //showing total headers
+  document.getElementsByClassName("totalRoute")[0].innerText = "Total Routes " + jsonData["routes"][0].route;
+  //now showing the header name with type
+//   we can take the reference of array querytype or queryName coz both are having same length
+RouteName.forEach((item, index)=>{
+    if(item=="")
+        item= "Name Not Provided"
+    if(RouteType[index]=="string")
+        className = "info"
+    else if(RouteType[index]== "int")
+        className ="secondary"
+    else if(RouteType[index]== "bool")
+        className = "success"
+    else
+        className = "danger"
+
+ 
+    document.getElementsByClassName("insertRouteinfo")[0].innerHTML += `
+    <div class="row mt-2">
+    <div class="col-sm-6">
+        <p><b> Route-${index+1} -- ${item} </b></p>
+    </div>
+    <div class="col-sm-6">
+        <button class="btn btn-${className}" style="height:40px; width:100px;">${RouteType[index]}</p>
+    </div>
+ </div>
+    `;
+})
+};
 
 
     //requestbody
