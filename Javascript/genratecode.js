38465dbf027447227dff37a8c03d2703e9cf5a38
namya <namyahs004@gmail.com>
2024-05-05 22:53:42 +0530

    added configuration.json structure

--- Javascript/genratecode.js
+++ Javascript/genratecode.js
@@ -13,3 +13,34 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+//giving mw json code
+var config = document.getElementsByClassName("Configuration")[0];
+var data =JSON.parse( localStorage.getItem("ApiData"));
+config.innerText = `{
+  "DownstreamScheme": "https",
+  "DownstreamPathTemplate": "${data.url}",
+  "UpstreamPathTemplate": "${data.url}",
+  "DownstreamHostAndPorts": [
+    {
+      "Host": "localhost",
+      "Port": 0
+    }
+  ],
+  "UpstreamHttpMethod": [
+    "GET"
+  ],
+  "FileCacheOptions": {
+    "TtlSeconds": 0,
+    "Region": "${data.Region}"
+  },
+  "RateLimitOptions": {
+    "EnableRateLimiting": false,
+    "Period": "1s",
+    "PeriodTimespan": 5,
+    "Limit": 3
+  },
+  "AuthenticationOptions": {
+    "AuthenticationProviderKey": "Bearer"
+  }
+},`
